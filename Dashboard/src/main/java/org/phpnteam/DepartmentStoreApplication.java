d34dcfbf5739fd7d840579a507df178b63d12893
Phuoc Nguyen <PhuocNguyen.081096@gmail.com>
2022-10-29 14:19:56 +0700

    phuoc_commit

---
+++ Dashboard/src/main/java/org/phpnteam/DepartmentStoreApplication.java
@@ -0,0 +1,13 @@
+package org.phpnteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentStoreApplication.class, args);
+    }
+
+}
